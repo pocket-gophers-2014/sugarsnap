525f261b4bee334ce26c270b6afdecc7f6a1c262
Dave Schaaf & Rex Suter <pair+dpschaaf.r3xy@devbootcamp.com>
2014-05-22 21:10:00 -0700

    Able to send a file to S3 from a form on the landing page. Using Amazon gem to generate form

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'rails', '3.2.18'
 gem 'sqlite3'
 gem 'pg'
 gem 'dotenv-rails', '~> 0.11.1'
+gem 'aws-sdk', '~> 1.0'
 
 
 # Gems used only for assets and not required
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def home
     @sugarsnap = 'Sugarsnap - A location based photo sharing app'
     @image = Image.new()
+    @form = Amazon.presigned_post
   end
 
 end
