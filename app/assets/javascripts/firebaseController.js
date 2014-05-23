b78b69c9f039961da3c2e3cb3389290476080879
Nick Meehan & robby <pair+nickmeehan.rbenapfl@devbootcamp.com>
2014-05-22 19:43:10 -0700

    added ability to sort initial photo set by createdAt time, with the most recent first.

--- app/assets/javascripts/firebaseController.js
+++ app/assets/javascripts/firebaseController.js
@@ -2,16 +2,20 @@ function FirebaseController(view, geo, coordinates) {
   this.view = view;
   this.geo = geo;
   this.coordinates = coordinates;
-  this.photos = []
   this.radius = 1;
 }
 
 FirebaseController.prototype = {
   init: function(array) {
-    //cut it down by last 10 based on timestamp!
-    this.photos = PhotoHandler.extractPhotos(array)
-    for (var i = 0; i < this.photos.length; i++) {
-      this.view.appendPhoto(this.photos[i])
+    console.log(array)
+    var photos = this.extractPhotos(array)
+    for (var i = 0; i < photos.length; i++) {
+      this.view.appendPhoto(photos[i])
     }
+  },
+  extractPhotos: function(array) {
+    var initialPhotos = PhotoHandler.getFirstTenPhotos(array)
+    var photoUrls = PhotoHandler.extractPhotoUrls(initialPhotos)
+    return photoUrls;
   }
 }
--- app/assets/javascripts/photoExtractor.js
+++ app/assets/javascripts/photoExtractor.js
@@ -1,8 +1,19 @@
 PhotoHandler = {
-  extractPhotos: function(photos) {
+  extractPhotoUrls: function(photos) {
     for (var i = 0; i < photos.length; i++) {
       photos[i] = photos[i].photoUrl
     }
     return photos
+  },
+  getFirstTenPhotos: function(photos) {
+    var sortedPhotos = this.sortByTimeCreated(photos)
+    var firstTenPhotos = sortedPhotos.slice(0,10);
+    return firstTenPhotos;
+  },
+  sortByTimeCreated: function(photos) {
+    var sortedPhotos = photos.sort(function(a, b) {
+      return b.createdAt - a.createdAt
+    })
+    return sortedPhotos;
   }
 }
