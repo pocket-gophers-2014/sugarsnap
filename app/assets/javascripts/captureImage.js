97516d21220b6378bd30339c2517645c20fe155e
Dave Schaaf & Rex Suter <pair+dpschaaf.r3xy@devbootcamp.com>
2014-05-22 17:00:02 -0700

    WIP: ajax call to post image to amazon

--- app/assets/javascripts/captureImage.js
+++ app/assets/javascripts/captureImage.js
@@ -1,5 +1,16 @@
-if (window.File && window.FileReader && window.FileList && window.Blob) {
-  alert("great success!")
-} else {
-  alert('The File APIs are not fully supported in this browser.')
+// if (window.File && window.FileReader && window.FileList && window.Blob) {
+//   alert("great success!")
+// } else {
+//   alert('The File APIs are not fully supported in this browser.')
+// }
+
+var formData = new FormData();
+for (var i = 0; i < files.length; i++) {
+  formData.append('file', files[i]);
+  var request = $.ajax({
+    url: 'https://s3.amazonaws.com/sugarsnapper/',
+    type: 'POST',
+    data: { file: files[i] }
+  }),
+  request.done(function(){ console.log('finished the upload') })
 }
