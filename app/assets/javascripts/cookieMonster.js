5e4cec4d6d1fb0de0f17496ee0f599fcf274191f
David Schaaf <david.p.schaaf@gmail.com>
2014-05-24 13:09:23 -0700

    adding a new cookie for browser session, set for 7 day expiration

--- app/assets/javascripts/cookieMonster.js
+++ app/assets/javascripts/cookieMonster.js
@@ -6,18 +6,23 @@ var cookieMonster = function () {
 cookieMonster.prototype = {
   expirationDate: function() {
     var now = new Date()
-    var expirationDay = now.getDay()+this.validDays
+    var expirationDay = now.getDate()+this.validDays
     var expiration = new Date(now.setDate(expirationDay)).toUTCString()
-    console.log(expiration)
     return expiration
   },
   cookieText: function (coord) {
-    var cookieText = "locations="+coord.toString()+'; expires='+this.expirationDate()+"; path=''"
+    console.log(this.expirationDate())
+    var cookieText = this.cookieName()+"="+cogord.toString()+"; path=/; expires="+this.expirationDate()+";"
     console.log(cookieText)
     return cookieText
   },
+  cookieName: function() {
+    return 'sugarsnap'+('000'+Math.floor(Math.random()*99998 + 1)).slice(-5)
+  },
   setCookie: function(coord) {
-    console.log(this.userCookie)
-    this.userCookie = this.cookieText(coord)
+    console.log(this.cookieText(coord))
+    document.cookie = this.cookieText(coord)
   }
 }
+
+
