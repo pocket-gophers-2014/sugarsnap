05391224842672ceec51e92f9efae5ceee5f5f21
robby <apprentice@dbc31.local>
2014-05-23 19:02:32 -0700

    jasmine tests for firebase controller

--- spec/javascripts/firebase_controller_spec.js
+++ spec/javascripts/firebase_controller_spec.js
@@ -1,6 +1,37 @@
-// describe("FirebaseController.extractInitialPhotos", function() {
-//   it("returns an array", function() {
-//     var firebaseController = new FirebaseController(new FirebaseView(), FirebaseConnection.getGeo(), [37.7845035, -122.3969726])
-//     expect(FirebaseConnection).toBe(defined());
-//   })
-// })
+describe('FirebaseController', function() {
+  var view = new FirebaseView()
+  var geo = 0
+  var coordinates = 0
+  var testController = new FirebaseController(view,geo,coordinates)
+  describe('init', function() {
+    it('calls appendPhotosToFeed', function() {
+      // var testController = new FirebaseController
+      spyOn(testController, 'appendPhotosToFeed')
+      spyOn(testController, 'extractInitialPhotos')
+      testController.init()
+      expect(testController.appendPhotosToFeed).toHaveBeenCalled()
+    })
+    it('calls extractInitialPhotos', function() {
+      // var testController = new FirebaseController
+      spyOn(testController, 'appendPhotosToFeed')
+      spyOn(testController, 'extractInitialPhotos')
+      testController.init()
+      expect(testController.extractInitialPhotos).toHaveBeenCalled()
+    })
+  })
+  describe('extractInitialPhotos', function() {
+    it('returns an array of photo urls from array of photo objects after sorting and slicing the array in the photoExtractor module', function() {
+      var testArray = [{photoUrl:'url',createdAt:0},{photoUrl:'url2',createdAt:1}]
+      expect(testController.extractInitialPhotos(testArray)).toEqual(['url'])
+    })
+  })
+  // describe('updatePhotoStream', function(){
+  //   it('calls prependNewPhoto', function() {
+  //     var testArray = [0]
+  //     spyOn(testController, 'testController.view.appendPhoto')
+  //     testController.appendPhotosToFeed(testArray)
+  //     expect(testController.view.appendPhoto).toHaveBeenCalled()
+  //   })
+  // })
+
+})
