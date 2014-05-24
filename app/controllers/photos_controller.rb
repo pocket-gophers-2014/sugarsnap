77df53fb1e4ac8113dcfa71991d84ac174608a6f
Dave Schaaf & Rex Suter <pair+dpschaaf.r3xy@devbootcamp.com>
2014-05-23 18:54:22 -0700

    retrieving url from photo, need to process on client end

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,7 +9,7 @@ class PhotosController < ApplicationController
     p params
     @photo = Photo.new(image: params[:photo][:image])
     if @photo.save
-      render json: { url: "find me on Amazon" }
+      render json: { url: @photo.public_url }
     else
       render json: { hey: @photo.errors.full_messages }
     end
