cec14ebe8cf5469c4a0475aa6a946ceeae2fe7ea
David Schaaf <david.p.schaaf@gmail.com>
2014-05-24 13:42:31 -0700

    cookie setter and getter

--- app/assets/javascripts/cookieMonster.js
+++ app/assets/javascripts/cookieMonster.js
@@ -1,27 +1,31 @@
-var cookieMonster = function () {
-  this.userCookie = document.cookie
-  this.validDays = 7
-}
-
-cookieMonster.prototype = {
+CookieSetter = {
+  validDays: function() {
+    return 7
+  },
   expirationDate: function() {
     var now = new Date()
-    var expirationDay = now.getDate()+this.validDays
+    var expirationDay = now.getDate()+this.validDays()
     var expiration = new Date(now.setDate(expirationDay)).toUTCString()
     return expiration
-  },
-  cookieText: function (coord) {
-    console.log(this.expirationDate())
-    var cookieText = this.cookieName()+"="+cogord.toString()+"; path=/; expires="+this.expirationDate()+";"
-    console.log(cookieText)
-    return cookieText
   },
   cookieName: function() {
     return 'sugarsnap'+('000'+Math.floor(Math.random()*99998 + 1)).slice(-5)
   },
+  cookieText: function (coord) {
+    var cookieText = this.cookieName()+"="+coord.toString()+"; path=/; expires="+this.expirationDate()+";"
+    return cookieText
+  },
   setCookie: function(coord) {
-    console.log(this.cookieText(coord))
+    this.readCookieCoordinates()
     document.cookie = this.cookieText(coord)
+  },
+  readCookieCoordinates: function() {
+    var cookie = document.cookie.split(';')
+    var coords = []
+    for (var i = 0; i < cookie.length; i++) {
+       coords.push(cookie.split('='))
+     };
+    console.log(coords)
   }
 }
 
