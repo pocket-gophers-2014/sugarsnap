533e185b4fcb065709d9ba27c21dab011eacf4f4
Nick Meehan & robby <pair+nickmeehan.rbenapfl@devbootcamp.com>
2014-05-24 15:13:14 -0700

    pages controller test

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -36,11 +36,3 @@ describe PhotosController do
     end
   end
 end
-
-
-# Parameters: {"photo"=>{"image"=>#<ActionDispatch::Http::UploadedFile:0x007f82a3a5cc88 @original_filename="awesome.jpg", @content_type="image/jpeg", @headers="Content-Disposition: form-data; name=\"photo[image]\"; filename=\"awesome.jpg\"\r\nContent-Type: image/jpeg\r\n", @tempfile=#<Tempfile:/var/folders/t9/wt5rqhvd7hz68_nt_5yfs79w0000gn/T/RackMultipart20140524-15910-1rk6slr>>}}
-
-
-# Parameters: {"photo"=>{"image"=>#<Rack::Test::UploadedFile:0x007fc1ec7f3770 @content_type="image/jpg", @original_filename="awesome.jpg", @tempfile=#<Tempfile:/var/folders/t9/wt5rqhvd7hz68_nt_5yfs79w0000gn/T/awesome.jpg20140524-21085-1eypulj>>}}
-
-# Parameters: {"photo"=>{"image"=>{"file"=>#<Rack::Test::UploadedFile:0x007f95f9f88888 @content_type="image/jpg", @original_filename="awesome.jpg", @tempfile=#<Tempfile:/var/folders/t9/wt5rqhvd7hz68_nt_5yfs79w0000gn/T/awesome.jpg20140524-20829-6iguub>>}}}
