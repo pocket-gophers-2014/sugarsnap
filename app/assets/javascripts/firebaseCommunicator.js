bef1c84e54700e50d19d028df15473e22ca2a62f
Nick Meehan & robby <pair+nickmeehan.rbenapfl@devbootcamp.com>
2014-05-25 15:02:05 -0700

    redo edit since off doesn't remove listener

--- app/assets/javascripts/firebaseCommunicator.js
+++ app/assets/javascripts/firebaseCommunicator.js
@@ -1,6 +1,6 @@
 var FirebaseCommunicator = {
   getInitialPhotos: function(controller) {
-    controller.geo.onPointsNearLoc(controller.coordinates, controller.radius, function(array) {
+    controller.geo.getPointsNearLoc(controller.coordinates, controller.radius, function(array) {
       controller.init(array);
     })
   },
