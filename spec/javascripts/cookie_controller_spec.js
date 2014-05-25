a2e5a963eb41c83c055cfc9cd7531bb7f2c88943
David Schaaf <david.p.schaaf@gmail.com>
2014-05-25 14:58:31 -0700

    adding jasmine testing for cookies

--- spec/javascripts/cookie_controller_spec.js
+++ spec/javascripts/cookie_controller_spec.js
@@ -1,26 +1,34 @@
-describe('CookieController', function() {
-  var cookieController;
-  var cookieSetter;
-  var cookieGetter;
+describe('CookieController', function () {
+  var cookieController
+  var cookieSetter
+  var cookieGetter
+  var newCoordinates
   beforeEach(function() {
-    cookieController =  CookieController
+    cookieController = CookieController
     cookieSetter = CookieSetter
     cookieGetter = CookieGetter
-  })
-  it('is defined', function() {
-    expect(cookieController).toBeDefined();
+    newCoordinates = [37.7844234,-122.3970566]
   });
   it('is defined', function() {
     expect(cookieController).toBeDefined();
   });
-  describe('CookieSetter', function() {
-    it('is defined', function() {
-      expect(cookieSetter).toBeDefined();
-    })
-  })
-  describe('CookieGetter', function() {
-    it('is defined', function() {
-      expect(cookieGetter).toBeDefined();
-    })
-  })
-})
+  it('has a function newLocation', function() {
+    expect(cookieController.newLocation).toBeDefined();
+  });
+  it('detects when a new location cookie should be added', function() {
+    expect(cookieController.newLocation).toBeDefined();
+  });
+
+
+
+  // describe('CookieSetter', function() {
+  //   it('is defined', function() {
+  //     expect(cookieSetter).toBeDefined();
+  //   });
+  // });
+  // describe('CookieGetter', function() {
+  //   it('is defined', function() {
+  //     expect(cookieGetter).toBeDefined();
+  //   });
+  // });
+});
