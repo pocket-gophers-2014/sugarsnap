68e53ff9686c70f64861e8372e661690f309ee84
Nick Meehan & Rex Suter <pair+nickmeehan.r3xy@devbootcamp.com>
2014-05-26 20:07:15 -0700

    Moved selectors out of module functions by wrapping it in an IIFE.
    Renamed img to imageClone.

--- app/assets/javascripts/spinner.js
+++ app/assets/javascripts/spinner.js
@@ -1,14 +1,20 @@
-SpinnerModule = {
-  renderSpinnerAnimation: function() {
-    var template = $('#feed')
-    var img = $('.hidden').clone()
-    img.removeClass('hidden')
-    img.attr('id', 'waiting')
-    template.prepend(img.attr('src', 'spinner.gif'))
-  },
-  removeSpinnerAnimation: function() {
-    if ($('#waiting')) {
-      $('#waiting').remove()
+var SpinnerModule = (function() {
+  var feedSelector = '#feed'
+  var hiddenSelector = '.hidden'
+  var waitingSelector = '#waiting'
+
+  return {
+    renderSpinnerAnimation: function() {
+      var $template = $(feedSelector)
+      var $imageClone = $(hiddenSelector).clone()
+      $imageClone.removeClass('hidden')
+      $imageClone.attr('id', 'waiting')
+      $template.prepend(imageClone.attr('src', 'spinner.gif'))
+    },
+    removeSpinnerAnimation: function() {
+      if ($(waitingSelector)) {
+        $(waitingSelector).remove()
+      }
     }
   }
-}
+})();
