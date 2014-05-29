bfbc6eb04ee0072219de96ef448fd6fadc1e52a7
David Schaaf <david.p.schaaf@gmail.com>
2014-05-29 08:24:16 -0700

    adding sugarsnap file

---
+++ app/assets/javascripts/sugarsnap.js
@@ -0,0 +1,21 @@
+SugarSnap = {
+  initialize: function() {
+    navigator.geolocation.getCurrentPosition(this.getCoordinatesSuccess, this.getCoordinatesFailure.bind(this))
+  },
+  getCoordinatesSuccess: function(position) {
+    SpinnerModule.renderSpinnerAnimation();
+    var firebaseController = new FirebaseController(position, FirebaseConnection.getGeo())
+    var photoController = new PhotoController(new PhotoView())
+    firebaseController.subscribeListenerForInitialPhotos(photoController)
+    firebaseController.subscribeListenerForLivePhotoUpdates(photoController)
+    new CameraController(new CameraView()).bindCameraListener(firebaseController)
+    new HeaderController(new HeaderView()).bindHeaderListener()
+    InfiniteScroller.checkScrollThreshold( photoController )
+    CookieController.manageCookies( [firebaseController.latitude, firebaseController.longitude], firebaseController.radius )
+    SubmissionModule.listenForFileUpload()
+  },
+  getCoordinatesFailure: function() {
+    alert("We're sorry,we couldn't find you! We'll keep searching.")
+    this.initialize()
+  }
+}
