3ea50e163895e6906dfd2c4ce5dddc259a835b86
David Schaaf <david.p.schaaf@gmail.com>
2014-06-07 08:49:58 -0700

    removed cosmetic change for presentation

--- app/assets/javascripts/sugarsnap.js
+++ app/assets/javascripts/sugarsnap.js
@@ -8,7 +8,16 @@ SugarSnap = {
     }
   },
   getCoordinatesSuccess: function(position) {
-    // initializes the application
+    SpinnerModule.renderSpinnerAnimation();
+    var firebaseController = new FirebaseController(position, FirebaseConnection.getGeo())
+    var photoController = new PhotoController(new PhotoView())
+    firebaseController.subscribeListenerForInitialPhotos(photoController)
+    firebaseController.subscribeListenerForLivePhotoUpdates(photoController)
+    new CameraController(new CameraView()).bindCameraListener(firebaseController)
+    new HeaderController(new HeaderView()).bindHeaderListener()
+    InfiniteScroller.checkScrollThreshold( photoController )
+    CookieController.manageCookies( [firebaseController.latitude, firebaseController.longitude], firebaseController.radius )
+    SubmissionModule.listenForFileUpload()
   },
   getCoordinatesFailure: function() {
     alert("We're sorry,we couldn't find you! We'll keep searching.")
